836ca4e3f7c94fdf8d0c2e1a9525a4b382269bc5
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-05-18 16:58:40 -0300

    começando projeto e definindo migrations

---
+++ src/database/migrations/2023_05_18_193643_create_slot_machine_bets_log_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('watanabet.slot_machine_bets_log', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained('watanabet.users');
+            $table->foreignId('slot_machine_results_id')->constrained('watanabet.slot_machine_results');
+            $table->decimal('bet_amount', 10, 2);
+            $table->decimal('win_amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('watanabet.slot_machine_bets_log');
+    }
+};
